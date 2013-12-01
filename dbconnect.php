a0e32bb5e28adb96757646afe5d84c678a82c5ec
momrpg <hunterd@rpglink.in>
2013-11-30 22:39:09 -0500

    Initial Upload

---
+++ dbconnect.php
@@ -0,0 +1,10 @@
+<?php
+//This file automatically created by installer.php on Mar 08, 2013 04:10 am
+$DB_HOST = 'localhost';
+$DB_USER = 'chaozgui_lotgd';
+$DB_PASS = 'callforchaozguild';
+$DB_NAME = 'chaozgui_lotgd';
+$DB_PREFIX = '';
+$DB_USEDATACACHE = 1;
+$DB_DATACACHEPATH = '/home/chaozgui/public_html/cache';
+?>
